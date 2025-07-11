db79c869b1564907ca16f8e0da90ec462fce46a7
hanjaeyeong <jay907212@gmail.com>
2025-07-11 21:30:56 +0900

    에러 수정

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -14,12 +14,12 @@ export default class GameScene extends Phaser.Scene {
     this.load.image('bogoseo', '/src/assets/monster/bogoseo.png');
     this.load.image('usb','/src/assets/weapon/usb.png');
     this.load.image('coffee','/src/assets/weapon/coffee.png');
+    this.load.image('player', 'src/assets/images/Player.png');
+
   }
 
   create() {
     // 가운데 player 생성
-    this.player = new Player(this, 400, 300);
-
     this.monsters = this.physics.add.group();
 
     this.bullets = this.physics.add.group();
@@ -30,6 +30,17 @@ export default class GameScene extends Phaser.Scene {
         callback: this.spawnRandomMonster,
         callbackScope: this
     });
+
+
+    this.cursors = this.input.keyboard.createCursorKeys()
+
+    // ✅ 화면 크기 받아와서 중앙 계산
+    const centerX = this.cameras.main.width / 2
+    const centerY = this.cameras.main.height / 2
+
+    this.player = new Player(this, centerX, centerY)
+  
+
   }
 
   spawnRandomMonster() {
@@ -45,7 +56,7 @@ export default class GameScene extends Phaser.Scene {
     this.monsters.add(monster);
 }
   update(time,delta) {
-    this.player.update(time);
+    this.player.update(this.cursors);
     this.monsters.children.iterate(monster => {
         if (monster && monster.update){
             monster.update();
@@ -53,28 +64,6 @@ export default class GameScene extends Phaser.Scene {
     }
     );
   }
-}import Player from '../objects/Player.js'
 
-export default class GameScene extends Phaser.Scene {
-  constructor() {
-    super({ key: 'GameScene' })
-  }
 
-  preload() {
-    this.load.image('player', 'src/assets/images/Player.png')
-  }
-
-  create() {
-    this.cursors = this.input.keyboard.createCursorKeys()
-
-    // ✅ 화면 크기 받아와서 중앙 계산
-    const centerX = this.cameras.main.width / 2
-    const centerY = this.cameras.main.height / 2
-
-    this.player = new Player(this, centerX, centerY)
-  }
-
-  update() {
-    this.player.update(this.cursors)
-  }
 }

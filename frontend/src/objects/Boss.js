f9bc47134ad24a05832725074e5c71a07b0de646
P4HB <0815yunje@gmail.com>
2025-07-16 16:51:00 +0900

    boss chat fixed

--- frontend/src/objects/Boss.js
+++ frontend/src/objects/Boss.js
@@ -34,7 +34,7 @@ export default class Boss extends Monster {
     this.hpText = scene.add.text(0, 0, '', {
       fontSize: '12px', fill: '#fff', fontFamily: 'Arial', stroke: '#000', strokeThickness: 2
     }).setOrigin(0.5, 1);
-    const depth = 1000;
+    const depth = 500;
     this.hpBar.setDepth(depth);
     this.hpBarBg.setDepth(depth);
     this.hpText.setDepth(depth);
